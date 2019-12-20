0d7ee3e76e10687bd657f9a52a4ced9ad962f465
IssamMlk <mallek@et.esiea.fr>
2019-12-20 21:37:12 +0100

    Activity2 amelioration

---
+++ app/src/main/java/com/example/projetmobile4a/View/YoutubeVideos.java
@@ -0,0 +1,17 @@
+package com.example.projetmobile4a.View;
+
+public class YoutubeVideos {
+
+    String videoUrl;
+
+    public YoutubeVideos(){
+
+    }
+
+    public YoutubeVideos(String videoUrl){this.videoUrl=videoUrl;}
+
+    public String getVideoUrl(){return videoUrl;}
+
+    public void setVideoUrl(String videoUrl){this.videoUrl=videoUrl;}
+
+}
